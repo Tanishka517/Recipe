89c85c8402838bd212ce8f6e109c4ff5adce9f3a
techfantictanishka <your.email@example.com>
2026-06-22 09:27:51 +0530

    Refactor: Move project files to root repository level

--- flavourverse/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-
-export default defineConfig({
-  base: '/Recipe/', // <-- Add this crucial line here!
-  plugins: [
-    tailwindcss(),
-  ],
-})
