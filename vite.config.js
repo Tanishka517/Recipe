3d161516b112bf14c12ada0d67ad716375a4d9d5
techfantictanishka <your.email@example.com>
2026-06-22 09:52:32 +0530

    Fix: Separate index.html structure from main.jsx script

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/Recipe/', // <-- Add this crucial line here!
+  base: '/Recipe/', 
   plugins: [
     tailwindcss(),
   ],
